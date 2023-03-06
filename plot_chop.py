4444911138bfa572c7b463d391f0a40c08c78117
kwieczorek <wieczorek.konrad@gmail.com>
2023-03-06 15:36:55 +0200

    added plotting all on one chart

--- plot_chop.py
+++ plot_chop.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Step 1: Load data from Excel file
-df = pd.read_excel('XAUUSD_choppiness_index_new_amended.xlsx')
+df = pd.read_excel('USDJPY_choppiness_index_new_amended.xlsx')
 
 # Step 2: Create a new column for color based on the 'trend' column
 df['color'] = df['trend'].map({0: 'black', 1: 'green'})
@@ -15,6 +15,9 @@ fig, ax = plt.subplots()
 grouped.plot(ax=ax, color=['black', 'green'])
 ax.set_xlabel('Date')
 ax.set_ylabel('Close')
+ax.legend(['Not trending', 'Trending'], loc='upper left')
+
+plt.savefig('USDJPY_trend.png')
 plt.show()
 
 
